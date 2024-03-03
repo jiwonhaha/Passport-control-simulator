0486f8a066399f6a3b3839bf0b27645b6724cd6c
Jiwon Park <jxp042@student.bham.ac.uk>
2024-03-03 14:51:14 +0000

    grabbing with ray incomplete

---
+++ Assets/XROffsetGradInteractable.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class XROffsetGrabInteractable : XRGrabInteractable
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(!attachTransform)
+        {
+            GameObject attachPoint = new GameObject("Offset Grab Pivot");
+            attachPoint.transform.SetParent(transform, false);
+            attachTransform = attachPoint.transform;
+        }
+    }
+
+    protected override void OnSelectEntered(SelectEnterEventArgs args)
+    {
+        attachTransform.position = args.interactorObject.transform.position;
+        attachTransform.rotation = args.interactorObject.transform.rotation;
+
+        base.OnSelectEntered(args);
+    }
+}
