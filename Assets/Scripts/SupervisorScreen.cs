ca1d2ed08480c2a0a15ee264f516424714e8a9df
Pruetikorn Chirattitikarn <Benzpreu@gmail.com>
2024-03-17 16:40:42 +0000

    Add visa free checklist

--- Assets/Scripts/SupervisorScreen.cs
+++ Assets/Scripts/SupervisorScreen.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Checklist : MonoBehaviour
+public class SupervisorScreen : MonoBehaviour
 {
     [SerializeField] GameObject player;
 
